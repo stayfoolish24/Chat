00e3526b86d4c8b207eb6b08ca647dc9306486c9
SeoulStore <stayfoolish24@gmail.com>
2019-01-13 23:45:17 +0900

    app.js 수정

--- App.js
+++ App.js
@@ -6,26 +6,57 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    'Shake or press menu button for dev menu'
+})
 
-type Props = {};
-export default class App extends Component<Props> {
+const baseURL = ''
+
+type Message = {
+  id: string,
+  body: string,
+  user: {
+    id: string,
+    name: string,
+    avatar: string
+  },
+  date: string
+}
+
+type State = {
+  messages: Array<Message>
+}
+type Props = {}
+export default class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      message: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(baseURL + '/messages')
+      .then(response => response.json())
+      .then(json => this.setState({ messages: json.messages }))
+      .catch(error => console.log(error))
+  }
+
   render() {
+    console.log(this.state.messages)
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -34,16 +65,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
